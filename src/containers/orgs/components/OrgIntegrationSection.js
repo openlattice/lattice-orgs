b3faf2301d571619e1a1dbef6c0eed766a4eb3c6
jokedurnez <joke.durnez@gmail.com>
2020-04-19 17:19:00 -0700

    indentation and format

--- src/containers/orgs/components/OrgIntegrationSection.js
+++ src/containers/orgs/components/OrgIntegrationSection.js
@@ -84,9 +84,7 @@ type State = {
 };
 
 class OrgIntegrationSection extends Component<Props, State> {
-
-  constructor(props :Props) {
-
+  constructor(props: Props) {
     super(props);
 
     this.state = {
@@ -95,150 +93,150 @@ class OrgIntegrationSection extends Component<Props, State> {
     };
   }
 
-  handleOnChangeForm = ({ formData } :{ formData :FormData }) => {
-
+  handleOnChangeForm = ({ formData }: { formData: FormData }) => {
     const { formData: stateFormData } = this.state;
 
     let newFormData = fromJS(formData);
     if (isNonEmptyString(formData.fields.targetDBMS)) {
-      if (stateFormData.getIn(['fields', 'targetDBMS']) !== newFormData.getIn(['fields', 'targetDBMS'])) {
-        const dbms :Object = DBMSTypes[formData.fields.targetDBMS];
-        newFormData = newFormData.setIn(['fields', 'targetPort'], dbms.port);
+      if (
+        stateFormData.getIn(["fields", "targetDBMS"]) !==
+        newFormData.getIn(["fields", "targetDBMS"])
+      ) {
+        const dbms: Object = DBMSTypes[formData.fields.targetDBMS];
+        newFormData = newFormData.setIn(["fields", "targetPort"], dbms.port);
       }
     }
 
     this.setState({ formData: newFormData });
-  }
+  };
 
   handleOnClickCopyCredential = () => {
-
     const { isOwner, org } = this.props;
 
     if (isOwner) {
       // TODO: consider using https://github.com/zenorocha/clipboard.js
       if (navigator.clipboard) {
-        navigator.clipboard.writeText(org.getIn(['integration', 'credential'], ''));
+        navigator.clipboard.writeText(
+          org.getIn(["integration", "credential"], "")
+        );
       }
     }
-  }
+  };
 
   handleOnClickGenerate = () => {
-
     const { org } = this.props;
     const { formData } = this.state;
 
     generateIntegrationConfigFile({
-      orgId: org.get('id'),
-      orgName: org.get('title'),
-      orgPassword: org.getIn(['integration', 'credential']),
-      orgUsername: org.getIn(['integration', 'user']),
-      targetDatabase: formData.getIn(['fields', 'targetDatabase']),
-      targetPort: formData.getIn(['fields', 'targetPort']),
-      targetServer: formData.getIn(['fields', 'targetServer']),
-      targetDBMS: formData.getIn(['fields', 'targetDBMS']),
+      orgId: org.get("id"),
+      orgName: org.get("title"),
+      orgPassword: org.getIn(["integration", "credential"]),
+      orgUsername: org.getIn(["integration", "user"]),
+      targetDatabase: formData.getIn(["fields", "targetDatabase"]),
+      targetPort: formData.getIn(["fields", "targetPort"]),
+      targetServer: formData.getIn(["fields", "targetServer"]),
+      targetDBMS: formData.getIn(["fields", "targetDBMS"]),
     });
-  }
+  };
 
   closeModal = () => {
-
     this.setState({
       formData: INITIAL_FORM_DATA,
       isVisibleGenerateConfigModal: false,
     });
-  }
+  };
 
   openModal = () => {
-
     this.setState({
       isVisibleGenerateConfigModal: true,
     });
-  }
+  };
 
   renderGenerateConfigModal = () => {
-
     const { formData, isVisibleGenerateConfigModal } = this.state;
 
     return (
       <Modal
-          isVisible={isVisibleGenerateConfigModal}
-          onClickPrimary={this.handleOnClickGenerate}
-          onClose={this.closeModal}
-          textPrimary="Generate"
-          textTitle="Generate Integration Configuration File"
-          viewportScrolling>
+        isVisible={isVisibleGenerateConfigModal}
+        onClickPrimary={this.handleOnClickGenerate}
+        onClose={this.closeModal}
+        textPrimary="Generate"
+        textTitle="Generate Integration Configuration File"
+        viewportScrolling
+      >
         <Form
-            formData={formData.toJS()}
-            hideSubmit
-            noPadding
-            onChange={this.handleOnChangeForm}
-            schema={dataSchema}
-            uiSchema={uiSchema} />
+          formData={formData.toJS()}
+          hideSubmit
+          noPadding
+          onChange={this.handleOnChangeForm}
+          schema={dataSchema}
+          uiSchema={uiSchema}
+        />
       </Modal>
     );
-  }
-renderDatabaseUrl = () => {
-  const { org } = this.props;
-  const orgIdClean = org.get('id').replace(/-/g, '');
+  };
+  renderDatabaseUrl = () => {
+    const { org } = this.props;
+    const orgIdClean = org.get("id").replace(/-/g, "");
 
-  return (
-    <SectionGrid>
-      <h2>Database Details</h2>
-      <h5>JDBC URL</h5>
-      <pre>{`jdbc:postgresql://atlas.openlattice.com:30001/org_${orgIdClean}`}</pre>
-    </SectionGrid>
-  );
-};
-
-renderDatabaseCredentials = () => {
-  const { isOwner, org } = this.props;
-
-  if (!isOwner) {
-    return null;
-  }
-
-  const integration:Map = org.get('integration', Map());
-  if (integration.isEmpty()) {
-    return null;
-  }
-
-  return (
-    <>
+    return (
       <SectionGrid>
-        <h5>USER</h5>
-        <pre>{org.getIn(["integration", "user"], "")}</pre>
-        <h5>CREDENTIAL</h5>
+        <h2>Database Details</h2>
+        <h5>JDBC URL</h5>
+        <pre>{`jdbc:postgresql://atlas.openlattice.com:30001/org_${orgIdClean}`}</pre>
       </SectionGrid>
-      <SectionGrid columns={2}>
-        <div style={{ marginTop: "4px" }}>
-          <ActionControlWithButton>
-            <Input
-              disabled
-              type="password"
-              value="********************************"
-            />
-            <CopyButton onClick={this.handleOnClickCopyCredential} />
-          </ActionControlWithButton>
-        </div>
-      </SectionGrid>
-      <SectionGrid columns={2}>
-        <div>
-          <Button mode="primary" onClick={this.openModal}>
-            Generate Integration Configuration File
-          </Button>
-        </div>
-      </SectionGrid>
-      {this.renderGenerateConfigModal()}
-    </>
-  );
+    );
+  };
 
-};
+  renderDatabaseCredentials = () => {
+    const { isOwner, org } = this.props;
 
-render() {
-  return (
-    <>
-    {this.renderDatabaseUrl()}
-    {this.renderDatabaseCredentials()}
-    </>
+    if (!isOwner) {
+      return null;
+    }
+
+    const integration: Map = org.get("integration", Map());
+    if (integration.isEmpty()) {
+      return null;
+    }
+
+    return (
+      <>
+        <SectionGrid>
+          <h5>USER</h5>
+          <pre>{org.getIn(['integration', 'user'], '')}</pre>
+          <h5>CREDENTIAL</h5>
+        </SectionGrid>
+        <SectionGrid columns={2}>
+          <div style={{ marginTop: '4px' }}>
+            <ActionControlWithButton>
+              <Input
+                disabled
+                type="password"
+                value="********************************"
+              />
+              <CopyButton onClick={this.handleOnClickCopyCredential} />
+            </ActionControlWithButton>
+          </div>
+        </SectionGrid>
+        <SectionGrid columns={2}>
+          <div>
+            <Button mode="primary" onClick={this.openModal}>
+              Generate Integration Configuration File
+            </Button>
+          </div>
+        </SectionGrid>
+        {this.renderGenerateConfigModal()}
+      </>
+    );
+  };
+
+  render() {
+    return (
+      <>
+        {this.renderDatabaseUrl()}
+        {this.renderDatabaseCredentials()}
+      </>
     );
   }
 }
