3f3ecd776fdc609e5c74b0cc3bd1592c5bbf1868
Hristo Oskov <hristo@openlattice.com>
2020-08-04 13:42:18 -0700

    assigned roles section

--- src/containers/orgs/OrgMemberContainer.js
+++ src/containers/orgs/OrgMemberContainer.js
@@ -4,15 +4,38 @@
 
 import React from 'react';
 
-import { Map } from 'immutable';
-import { AppContentWrapper } from 'lattice-ui-kit';
+import styled from 'styled-components';
+import { List, Map } from 'immutable';
+import {
+  AppContentWrapper,
+  Card,
+  CardSegment,
+  CardStack,
+} from 'lattice-ui-kit';
 import type { Organization, UUID } from 'lattice';
 
-import { CrumbItem, CrumbLink, Crumbs } from '../../components';
+import {
+  CrumbItem,
+  CrumbLink,
+  Crumbs,
+  Header,
+} from '../../components';
 import { Routes } from '../../core/router';
 import { PersonUtils } from '../../utils';
 
-const { getUserProfileLabel } = PersonUtils;
+const {
+  getUserId,
+  getUserProfile,
+  getUserProfileLabel,
+} = PersonUtils;
+
+const Section = styled.section`
+  margin-top: 48px;
+`;
+
+const RoleTitle = styled.span`
+  line-height: 32px;
+`;
 
 type Props = {
   member :Map;
@@ -23,15 +46,50 @@ type Props = {
 const OrgMemberContainer = ({ member, organization, organizationId } :Props) => {
 
   const orgPath = Routes.ORG.replace(Routes.ORG_ID_PARAM, organizationId);
+
   const userProfileLabel = getUserProfileLabel(member);
+  const userId = getUserId(member);
+  const profile = getUserProfile(member);
+  const memberNameTag = profile.name || userProfileLabel || userId;
+
+  const roles = member
+    .get('roles', List())
+    .filter((role :Map) => role.get('organizationId') === organizationId);
 
   return (
     <AppContentWrapper>
       <Crumbs>
         <CrumbLink to={orgPath}>{organization.title || 'Organization'}</CrumbLink>
         <CrumbLink to={orgPath}>People</CrumbLink>
-        <CrumbItem>{userProfileLabel}</CrumbItem>
+        <CrumbItem>{memberNameTag}</CrumbItem>
       </Crumbs>
+      <Header as="h2">{memberNameTag}</Header>
+      <Section>
+        <Header as="h3">Assigned Roles</Header>
+        {
+          roles.isEmpty() && (
+            <i>No assigned roles</i>
+          )
+        }
+        {
+          !roles.isEmpty() && (
+            <CardStack>
+              {
+                roles.map((role :Map) => (
+                  <Card>
+                    <CardSegment padding="8px 16px" vertical={false}>
+                      <RoleTitle>{role.get('title')}</RoleTitle>
+                    </CardSegment>
+                  </Card>
+                ))
+              }
+            </CardStack>
+          )
+        }
+      </Section>
+      <Section>
+        <Header as="h3">Accessible Data Sets</Header>
+      </Section>
     </AppContentWrapper>
   );
 };
