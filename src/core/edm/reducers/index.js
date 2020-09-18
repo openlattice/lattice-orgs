d127c7f0f74682072b0ce9770e965340f5dd7ba9
Hristo Oskov <hristo@openlattice.com>
2020-09-17 23:40:37 -0700

    remove reducer file, use index instead

--- src/core/edm/reducers/index.js
+++ src/core/edm/reducers/index.js
@@ -2,8 +2,70 @@
  * @flow
  */
 
-import reducer from './reducer';
+import { List, Map, fromJS } from 'immutable';
+import { EntitySetsApiActions } from 'lattice-sagas';
 
-export { default as getEntityDataModelTypesReducer } from './getEntityDataModelTypesReducer';
+import getEntityDataModelTypesReducer from './getEntityDataModelTypesReducer';
+import getEntitySetReducer from './getEntitySetReducer';
+import getEntitySetsReducer from './getEntitySetsReducer';
+import getOrSelectEntitySetsReducer from './getOrSelectEntitySetsReducer';
 
-export default reducer;
+import {
+  ENTITY_SETS,
+  ENTITY_SETS_INDEX_MAP,
+  ENTITY_TYPES,
+  ENTITY_TYPES_INDEX_MAP,
+  PROPERTY_TYPES,
+  PROPERTY_TYPES_INDEX_MAP,
+  RS_INITIAL_STATE,
+} from '../../redux/constants';
+import {
+  GET_EDM_TYPES,
+  GET_OR_SELECT_ENTITY_SETS,
+  getEntityDataModelTypes,
+  getOrSelectEntitySets,
+} from '../actions';
+
+const {
+  GET_ENTITY_SET,
+  GET_ENTITY_SETS,
+  getEntitySet,
+  getEntitySets,
+} = EntitySetsApiActions;
+
+const INITIAL_STATE :Map = fromJS({
+  // actions
+  [GET_EDM_TYPES]: RS_INITIAL_STATE,
+  [GET_ENTITY_SET]: RS_INITIAL_STATE,
+  [GET_ENTITY_SETS]: RS_INITIAL_STATE,
+  [GET_OR_SELECT_ENTITY_SETS]: RS_INITIAL_STATE,
+  // data
+  [ENTITY_SETS]: List(),
+  [ENTITY_SETS_INDEX_MAP]: Map(),
+  [ENTITY_TYPES]: List(),
+  [ENTITY_TYPES_INDEX_MAP]: Map(),
+  [PROPERTY_TYPES]: List(),
+  [PROPERTY_TYPES_INDEX_MAP]: Map(),
+});
+
+export default function reducer(state :Map = INITIAL_STATE, action :Object) {
+
+  if (action.type === getEntityDataModelTypes.case(action.type)) {
+    return getEntityDataModelTypesReducer(state, action);
+  }
+
+  if (action.type === getEntitySet.case(action.type)) {
+    return getEntitySetReducer(state, action);
+  }
+
+  // console.log(action.type);
+  if (action.type === getEntitySets.case(action.type)) {
+    return getEntitySetsReducer(state, action);
+  }
+
+  if (action.type === getOrSelectEntitySets.case(action.type)) {
+    return getOrSelectEntitySetsReducer(state, action);
+  }
+
+  return state;
+}
--- src/core/permissions/reducers/index.js
+++ src/core/permissions/reducers/index.js
@@ -2,8 +2,34 @@
  * @flow
  */
 
-import reducer from './reducer';
+import { Map, fromJS } from 'immutable';
 
-export { default as gatherOrganizationPermissionsReducer } from './gatherOrganizationPermissionsReducer';
+import gatherOrganizationPermissionsReducer from './gatherOrganizationPermissionsReducer';
 
-export default reducer;
+import { RESET_REQUEST_STATE } from '../../redux/actions';
+import { ACES, RS_INITIAL_STATE } from '../../redux/constants';
+import { resetRequestStateReducer } from '../../redux/reducers';
+import {
+  GATHER_ORGANIZATION_PERMISSIONS,
+  gatherOrganizationPermissions,
+} from '../actions';
+
+const INITIAL_STATE :Map = fromJS({
+  // actions
+  [GATHER_ORGANIZATION_PERMISSIONS]: RS_INITIAL_STATE,
+  // data
+  [ACES]: Map(),
+});
+
+export default function reducer(state :Map = INITIAL_STATE, action :Object) {
+
+  if (action.type === RESET_REQUEST_STATE) {
+    return resetRequestStateReducer(state, action);
+  }
+
+  if (action.type === gatherOrganizationPermissions.case(action.type)) {
+    return gatherOrganizationPermissionsReducer(state, action);
+  }
+
+  return state;
+}
