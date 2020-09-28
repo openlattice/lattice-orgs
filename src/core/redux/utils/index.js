17239d69a514ca81d413f7dc79b1c6574e7504c1
Hristo Oskov <hristo@openlattice.com>
2020-09-28 15:58:16 -0700

    export selectOrganizationAtlasDataSetIds

--- src/core/redux/utils/index.js
+++ src/core/redux/utils/index.js
@@ -4,6 +4,7 @@
 
 export { default as selectEntitySetEntityType } from './selectEntitySetEntityType';
 export { default as selectEntitySetPropertyTypes } from './selectEntitySetPropertyTypes';
+export { default as selectOrganizationAtlasDataSetIds } from './selectOrganizationAtlasDataSetIds';
 export { default as selectOrganizationEntitySetIds } from './selectOrganizationEntitySetIds';
 export { default as selectOrganizationMembers } from './selectOrganizationMembers';
 export { default as selectPermissions } from './selectPermissions';
