d4bd4a171e6e83cceecea61141e718bca9092274
Hristo Oskov <hristo@openlattice.com>
2020-09-28 10:29:22 -0700

    fix flow

--- src/core/redux/utils/selectEntitySetEntityType.js
+++ src/core/redux/utils/selectEntitySetEntityType.js
@@ -16,6 +16,7 @@ export default function selectEntitySetEntityType(idOrEntitySet :UUID | EntitySe
 
     let entitySetId :?UUID;
     if (isValidUUID(idOrEntitySet)) {
+      // $FlowFixMe
       entitySetId = idOrEntitySet;
     }
     // $FlowFixMe
