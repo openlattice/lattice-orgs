f0904377f0a6a31bbd7c47dc7b077e2d5ecb53f7
Hristo Oskov <hristo@openlattice.com>
2020-09-29 17:35:23 -0700

    use constants

---
+++ src/utils/constants/index.js
@@ -0,0 +1,11 @@
+/*
+ * @flow
+ */
+
+const PRINCIPAL :'principal' = 'principal';
+const SECURABLE_PRINCIPAL_CLASS = 'com.openlattice.authorization.SecurablePrincipal';
+
+export {
+  PRINCIPAL,
+  SECURABLE_PRINCIPAL_CLASS,
+};
--- src/utils/getPrincipal.js
+++ src/utils/getPrincipal.js
@@ -5,6 +5,8 @@
 import { get, getIn, has } from 'immutable';
 import { Constants, Models } from 'lattice';
 
+import { PRINCIPAL, SECURABLE_PRINCIPAL_CLASS } from './constants';
+
 const { AT_CLASS } = Constants;
 const { Principal, PrincipalBuilder } = Models;
 
@@ -12,15 +14,15 @@ export default function getPrincipal(value :any) :?Principal {
 
   try {
     // com.openlattice.authorization.SecurablePrincipal
-    const securablePrincipalClass = getIn(value, ['principal', AT_CLASS]);
-    if (securablePrincipalClass === 'com.openlattice.authorization.SecurablePrincipal') {
-      const principal = getIn(value, ['principal', 'principal']);
+    const securablePrincipalClass = getIn(value, [PRINCIPAL, AT_CLASS]);
+    if (securablePrincipalClass === SECURABLE_PRINCIPAL_CLASS) {
+      const principal = getIn(value, [PRINCIPAL, PRINCIPAL]);
       return (new PrincipalBuilder(principal)).build();
     }
 
     // ???
-    if (has(value, 'principal')) {
-      const principal = get(value, 'principal');
+    if (has(value, PRINCIPAL)) {
+      const principal = get(value, PRINCIPAL);
       return (new PrincipalBuilder(principal)).build();
     }
 
