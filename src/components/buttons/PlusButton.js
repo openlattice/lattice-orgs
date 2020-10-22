8ff16e546935ad25e0360bfb1711627e6d75229d
Hristo Oskov <hristo@openlattice.com>
2020-10-22 13:05:08 -0700

    BaseButton

--- src/components/buttons/PlusButton.js
+++ src/components/buttons/PlusButton.js
@@ -5,26 +5,15 @@
 import React from 'react';
 
 import { faPlus } from '@fortawesome/pro-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Spinner } from 'lattice-ui-kit';
 
-// 2020-07-17 NOTE: using "disabled" prop instead of "isLoading" because of a margin bug in LUK
-const PlusButton = ({
-  color = 'success',
-  disabled = false,
-  isLoading = false,
-  variant = 'outlined',
-  ...props
-} :Object) => (
+import BaseButton from './BaseButton';
+import type { ButtonProps } from './BaseButton';
+
+const PlusButton = ({ children, ...props } :ButtonProps) => (
   /* eslint-disable react/jsx-props-no-spreading */
-  <Button {...props} color={color} disabled={disabled || isLoading} variant={variant}>
-    {
-      isLoading && <Spinner />
-    }
-    {
-      !isLoading && <FontAwesomeIcon fixedWidth icon={faPlus} />
-    }
-  </Button>
+  <BaseButton {...props} icon={faPlus}>
+    {children}
+  </BaseButton>
   /* eslint-enable */
 );
 
--- src/components/buttons/SearchButton.js
+++ src/components/buttons/SearchButton.js
@@ -5,24 +5,15 @@
 import React from 'react';
 
 import { faSearch } from '@fortawesome/pro-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Spinner } from 'lattice-ui-kit';
 
-// 2020-07-17 NOTE: using "disabled" prop instead of "isLoading" because of a margin bug in LUK
-const SearchButton = ({
-  disabled = false,
-  isLoading = false,
-  ...props
-} :Object) => (
+import BaseButton from './BaseButton';
+import type { ButtonProps } from './BaseButton';
+
+const SearchButton = ({ children, ...props } :ButtonProps) => (
   /* eslint-disable react/jsx-props-no-spreading */
-  <Button {...props} disabled={disabled || isLoading}>
-    {
-      isLoading && <Spinner />
-    }
-    {
-      !isLoading && <FontAwesomeIcon fixedWidth icon={faSearch} />
-    }
-  </Button>
+  <BaseButton {...props} icon={faSearch}>
+    {children}
+  </BaseButton>
   /* eslint-enable */
 );
 
