4723233ee90e12427347f1543cf93a04433928bd
Hristo Oskov <hristo@openlattice.com>
2020-10-31 12:51:55 -0700

    searchOrganizationDataSets - temporary fix to work for non-shiproom orgs

--- src/core/search/sagas/searchOrganizationDataSets.js
+++ src/core/search/sagas/searchOrganizationDataSets.js
@@ -3,12 +3,14 @@
  */
 
 import { call, put, takeEvery } from '@redux-saga/core/effects';
+import { Set } from 'immutable';
 import { DataUtils, Logger, ValidationUtils } from 'lattice-utils';
 import type { Saga } from '@redux-saga/core';
 import type { UUID } from 'lattice';
 import type { WorkerResponse } from 'lattice-sagas';
 import type { SequenceAction } from 'redux-reqseq';
 
+import { searchDataSetsWorker } from './searchDataSets';
 import { searchEntitySetWorker } from './searchEntitySet';
 
 import { getOrSelectDataSets } from '../../edm/actions';
@@ -22,11 +24,16 @@ import {
 } from '../../redux/constants';
 import {
   SEARCH_ORGANIZATION_DATA_SETS,
+  searchDataSets,
   searchEntitySet,
   searchOrganizationDataSets,
 } from '../actions';
 
 const LOG = new Logger('SearchSagas');
+
+// TODO: DELETE ONCE PROPERLY IMPLEMENTED
+const SHIP_ROOM_ORG_ID = '81999873-5b22-434e-be9b-1f98931ae2e4';
+const SR_DS_META_ESID = '091695e1-a971-40ee-9956-a6a05c5942dd';
 
 const { getPropertyValue } = DataUtils;
 const { isValidUUID } = ValidationUtils;
@@ -37,19 +44,43 @@ function* searchOrganizationDataSetsWorker(action :SequenceAction) :Saga<*> {
     yield put(searchOrganizationDataSets.request(action.id, action.value));
 
     const organizationId :UUID = action.value.organizationId;
+    let atlasDataSetIds :Set<UUID> = Set();
+    let entitySetIds :Set<UUID> = Set();
 
-    const response :WorkerResponse = yield call(searchEntitySetWorker, searchEntitySet(action.value));
-    if (response.error) throw response.error;
+    let response :WorkerResponse;
 
-    const entitySetIds = response.data.hits
-      .filter((hit) => getPropertyValue(hit, [FQNS.OL_STANDARDIZED, 0]) === true)
-      .map((hit) => getPropertyValue(hit, [FQNS.OL_ID, 0]))
-      .filter((id :UUID) => isValidUUID(id));
+    if (organizationId === SHIP_ROOM_ORG_ID) {
 
-    const atlasDataSetIds = response.data.hits
-      .filter((hit) => getPropertyValue(hit, [FQNS.OL_STANDARDIZED, 0]) === false)
-      .map((hit) => getPropertyValue(hit, [FQNS.OL_ID, 0]))
-      .filter((id :UUID) => isValidUUID(id));
+      response = yield call(
+        searchEntitySetWorker, searchEntitySet({
+          ...action.value,
+          entitySetId: SR_DS_META_ESID,
+        })
+      );
+      if (response.error) throw response.error;
+
+      const entitySetIdsHits = response.data.hits
+        // .filter((hit) => getPropertyValue(hit, [FQNS.OL_STANDARDIZED, 0]) === true)
+        .filter((hit) => !Number.isInteger(getPropertyValue(hit, ['ol.pgoid', 0])))
+        .map((hit) => getPropertyValue(hit, [FQNS.OL_ID, 0]))
+        .filter((id :UUID) => isValidUUID(id));
+
+      entitySetIds = Set(entitySetIdsHits);
+
+      const atlasDataSetIdsHits = response.data.hits
+        // .filter((hit) => getPropertyValue(hit, [FQNS.OL_STANDARDIZED, 0]) === false)
+        .filter((hit) => Number.isInteger(getPropertyValue(hit, ['ol.pgoid', 0])))
+        .map((hit) => getPropertyValue(hit, [FQNS.OL_ID, 0]))
+        .filter((id :UUID) => isValidUUID(id));
+
+      atlasDataSetIds = Set(atlasDataSetIdsHits);
+    }
+    else {
+      // TODO: this will be removed
+      response = yield call(searchDataSetsWorker, searchDataSets(action.value));
+      if (response.error) throw response.error;
+      entitySetIds = response.data.hits;
+    }
 
     yield call(
       getOrSelectDataSetsWorker,
