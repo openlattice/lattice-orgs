00792432aa7611a1cda4c44e1d6f5b486c077542
Solomon Tang <solomoncztang@gmail.com>
2020-11-02 13:04:57 -0800

    debounce search

--- src/containers/org/components/SearchMemberModalBody.js
+++ src/containers/org/components/SearchMemberModalBody.js
@@ -1,19 +1,30 @@
 // @flow
 import React from 'react';
 
+import debounce from 'lodash/debounce';
 import { faSearch } from '@fortawesome/pro-light-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { PrincipalsApiActions } from 'lattice-sagas';
 import { Button, Select, Typography } from 'lattice-ui-kit';
+import { useDispatch } from 'react-redux';
 
 import { ModalBody } from '../../../components';
 
+const { SEARCH_ALL_USERS, searchAllUsers } = PrincipalsApiActions;
+
 const SearchMemberModalBody = () => {
+
+  const dispatch = useDispatch();
 
   const handleOnClick = (e :SyntheticEvent<HTMLButtonElement>) => {
     e.preventDefault();
     console.log('enter', e.currentTarget.value);
 
   };
+
+  const debounceDispatchSearch = debounce((value) => {
+    dispatch(searchAllUsers(value));
+  }, 250);
 
   return (
     <ModalBody>
@@ -22,6 +33,7 @@ const SearchMemberModalBody = () => {
         <Select
             hideDropdownIcon
             inputIcon={<FontAwesomeIcon icon={faSearch} />}
+            onInputChange={debounceDispatchSearch}
             options={[{ label: 'something', value: 'something' }]} />
         <Button type="submit" onClick={handleOnClick} />
       </form>
