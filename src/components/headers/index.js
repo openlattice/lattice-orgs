02a9e17a3597b3d537182c4535f4d002ab01d798
Hristo Oskov <hristo@openlattice.com>
2020-11-02 23:32:22 -0800

    reusable DataSetTitle component

--- src/components/headers/index.js
+++ src/components/headers/index.js
@@ -2,4 +2,5 @@
  * @flow
  */
 
+export { default as DataSetTitle } from './DataSetTitle';
 export { default as Header } from './Header';
--- src/containers/org/components/DataSetSearchResultCard.js
+++ src/containers/org/components/DataSetSearchResultCard.js
@@ -4,31 +4,15 @@
 
 import React from 'react';
 
-import styled from 'styled-components';
-import { faServer } from '@fortawesome/pro-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Map, getIn } from 'immutable';
 import { Card, CardSegment, Typography } from 'lattice-ui-kit';
 import { useGoToRoute } from 'lattice-utils';
 import { useSelector } from 'react-redux';
 import type { EntitySet, UUID } from 'lattice';
 
-import { EntitySetIcon } from '../../../assets/svg/icons';
-import { StackGrid } from '../../../components';
+import { DataSetTitle, StackGrid } from '../../../components';
 import { selectAtlasDataSets, selectEntitySets } from '../../../core/redux/selectors';
 import { Routes } from '../../../core/router';
-
-// TODO: move into src/components
-const TitleWrapper = styled.div`
-  align-items: center;
-  display: grid;
-  grid-gap: 16px;
-  grid-template-columns: auto 1fr;
-
-  > span {
-    word-break: break-all;
-  }
-`;
 
 const DataSetSearchResultCard = ({
   dataSetId,
@@ -58,14 +42,7 @@ const DataSetSearchResultCard = ({
     <Card id={dataSetId} onClick={goToOrganizationDataSet}>
       <CardSegment>
         <StackGrid gap={8}>
-          <TitleWrapper>
-            {
-              atlasDataSet
-                ? <FontAwesomeIcon fixedWidth icon={faServer} />
-                : <EntitySetIcon />
-            }
-            <Typography component="h2" variant="h4">{title || name}</Typography>
-          </TitleWrapper>
+          <DataSetTitle component="h2" isAtlasDataSet={!!atlasDataSet} variant="h4">{title || name}</DataSetTitle>
           <Typography>{description || name}</Typography>
         </StackGrid>
       </CardSegment>
