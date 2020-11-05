463dbfe8265c8494e6caf96c8a689b3dae5d212f
Solomon Tang <solomoncztang@gmail.com>
2020-11-04 18:05:53 -0800

    add role icon

--- src/assets/svg/icons/index.js
+++ src/assets/svg/icons/index.js
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 
 import EntitySetIconSVG from './entity-set-icon.svg';
 import OpenLatticeIconSVG from './ol-icon.svg';
+import RoleIconSVG from './role-icon.svg';
 
 const EntitySetIcon = styled.img.attrs({
   alt: 'entity-set-icon',
@@ -17,9 +18,16 @@ const OpenLatticeIcon = styled.img.attrs({
   src: OpenLatticeIconSVG,
 })``;
 
+const RoleIcon = styled.img.attrs({
+  alt: 'role-icon',
+  src: RoleIconSVG,
+})``;
+
 export {
   EntitySetIcon,
   EntitySetIconSVG,
   OpenLatticeIcon,
   OpenLatticeIconSVG,
+  RoleIcon,
+  RoleIconSVG,
 };
