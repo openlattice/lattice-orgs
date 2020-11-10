4d6724879ce557a58db7749f5d53ddb08dbed4c3
Hristo Oskov <hristo@openlattice.com>
2020-11-10 14:20:18 -0800

    revert changes to ModalBody

--- src/components/modals/ModalBody.js
+++ src/components/modals/ModalBody.js
@@ -2,42 +2,16 @@
  * @flow
  */
 
-import React, { useEffect } from 'react';
+import type { ComponentType } from 'react';
 
-import _isFunction from 'lodash/isFunction';
 import styled from 'styled-components';
 
-const ModalBodyWrapper = styled.div`
+const ModalBody :ComponentType<{|
+  children ?:any;
+  width ?:string;
+|}> = styled.div`
   max-width: 100%;
   width: ${({ width }) => ((typeof width === 'number' && width > 0) ? width : 720)}px;
 `;
-
-const ModalBody = ({
-  children,
-  onCleanUp,
-  width,
-} :{|
-  children :any;
-  onCleanUp ?:() => void;
-  width ?:number;
-|}) => {
-
-  useEffect(() => () => {
-    if (_isFunction(onCleanUp)) {
-      onCleanUp();
-    }
-  }, [onCleanUp]);
-
-  return (
-    <ModalBodyWrapper width={width}>
-      {children}
-    </ModalBodyWrapper>
-  );
-};
-
-ModalBody.defaultProps = {
-  onCleanUp: undefined,
-  width: 720,
-};
 
 export default ModalBody;
