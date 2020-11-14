28aeee916d472932d638bd1e0cd4bd8e232bf74c
Solomon Tang <solomoncztang@gmail.com>
2020-11-13 19:59:48 -0800

    description modal

--- src/containers/org/components/OrgDescriptionModal.js
+++ src/containers/org/components/OrgDescriptionModal.js
@@ -1,8 +1,16 @@
 // @flow
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { Modal, ModalFooter, ModalHeader } from 'lattice-ui-kit';
-import type { Organization, UUID } from 'lattice';
+import { Modal } from 'lattice-ui-kit';
+import { useRequestState } from 'lattice-utils';
+import { RequestStates } from 'redux-reqseq';
+import type { Organization } from 'lattice';
+import type { RequestState } from 'redux-reqseq';
+
+import EditDescriptionBody from './EditDescriptionBody';
+
+import { ORGANIZATIONS } from '../../../core/redux/constants';
+import { EDIT_ORGANIZATION_DETAILS } from '../actions';
 
 type Props = {
   isVisible :boolean;
@@ -15,13 +23,24 @@ const OrgDescriptionModal = ({
   onClose,
   organization,
 } :Props) => {
+
+  const requestState :?RequestState = useRequestState([ORGANIZATIONS, EDIT_ORGANIZATION_DETAILS]);
+
+  useEffect(() => {
+    if (requestState === RequestStates.SUCCESS) {
+      onClose();
+    }
+  }, [onClose, requestState]);
+
   return (
     <Modal
-      isVisible={isVisible}
-      onClose={onClose}
-      textTitle="Edit Description"
-      viewportScrolling
-      withFooter={false} />
+        isVisible={isVisible}
+        onClose={onClose}
+        textTitle="Edit Description"
+        viewportScrolling
+        withFooter={false}>
+      <EditDescriptionBody organization={organization} requestState={requestState} />
+    </Modal>
   );
 };
 
