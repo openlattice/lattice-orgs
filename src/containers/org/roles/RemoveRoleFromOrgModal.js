7f6bc72516d5b47531c8ef1c4fee4e2fc97dae92
Todd Bergman <todd@openlattice.com>
2020-12-14 14:37:12 -0800

    use ResetOnUnmount

--- src/containers/org/roles/RemoveRoleFromOrgModal.js
+++ src/containers/org/roles/RemoveRoleFromOrgModal.js
@@ -1,19 +1,15 @@
 // @flow
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { ActionModal } from 'lattice-ui-kit';
-import { useRequestState } from 'lattice-utils';
+import { Modal } from 'lattice-ui-kit';
 import { useDispatch } from 'react-redux';
-import { RequestStates } from 'redux-reqseq';
 import type { Organization, Role, UUID } from 'lattice';
-import type { RequestState } from 'redux-reqseq';
 
-import { ModalBody } from '../../../components';
-import { resetRequestState } from '../../../core/redux/actions';
-import { goToRoute } from '../../../core/router/actions';
-import { ORG_ID_PARAM, ORG_MEMBERS } from '../../../core/router/Routes';
-import { ORGANIZATIONS } from '../../../core/redux/constants';
+import ResetOnUnmount from '../components/ResetOnUnmount';
 import { REMOVE_ROLE_FROM_ORGANIZATION, removeRoleFromOrganization } from '../actions';
+
+
+const resetStatePath = [REMOVE_ROLE_FROM_ORGANIZATION];
 
 type Props = {
   isVisible :boolean;
@@ -31,26 +27,6 @@ const RoleDetailsModal = ({
   const dispatch = useDispatch();
   const roleId :UUID = role?.id || '';
   const organizationId :string = organization?.id || '';
-  const requestState :?RequestState = useRequestState([ORGANIZATIONS, REMOVE_ROLE_FROM_ORGANIZATION]);
-  const rolePath = ORG_MEMBERS.replace(ORG_ID_PARAM, organizationId);
-
-  const rsComponents = {
-    [RequestStates.STANDBY]: (
-      <ModalBody>
-        <span>Are you sure you want to delete this role? This action cannot be undone.</span>
-      </ModalBody>
-    ),
-    [RequestStates.SUCCESS]: (
-      <ModalBody>
-        <span>This role has been deleted.</span>
-      </ModalBody>
-    ),
-    [RequestStates.FAILURE]: (
-      <ModalBody>
-        <span>Failed to remove role. Please try again.</span>
-      </ModalBody>
-    ),
-  };
 
   const handleOnClickPrimary = () => {
     dispatch(
@@ -59,28 +35,19 @@ const RoleDetailsModal = ({
         roleId,
       })
     );
-    dispatch(goToRoute(rolePath));
   };
 
-  useEffect(() => {
-    if (requestState === RequestStates.SUCCESS) {
-      onClose();
-      // the timeout avoids rendering the modal with new state before the transition animation finishes
-      setTimeout(() => {
-        dispatch(resetRequestState([REMOVE_ROLE_FROM_ORGANIZATION]));
-      }, 1000);
-    }
-  }, [dispatch, requestState, rolePath, onClose]);
-
   return (
-    <ActionModal
+    <Modal
         isVisible={isVisible}
         textPrimary="Delete"
         onClickPrimary={handleOnClickPrimary}
         onClose={onClose}
-        requestState={requestState}
-        requestStateComponents={rsComponents}
-        textTitle="Delete Role" />
+        textTitle="Delete Role">
+      <ResetOnUnmount
+          path={resetStatePath}
+          message="Are you sure you want to delete this role? This action cannot be undone." />
+    </Modal>
   );
 };
 
