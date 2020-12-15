26e9b4d8ac7a5e60695c90cc7778670623985275
Todd Bergman <todd@openlattice.com>
2020-12-15 11:31:48 -0800

    remove blank line

--- src/containers/org/roles/RemoveRoleFromOrgModal.js
+++ src/containers/org/roles/RemoveRoleFromOrgModal.js
@@ -8,7 +8,6 @@ import type { UUID } from 'lattice';
 import ResetOnUnmount from '../components/ResetOnUnmount';
 import { REMOVE_ROLE_FROM_ORGANIZATION, removeRoleFromOrganization } from '../actions';
 
-
 const resetStatePath = [REMOVE_ROLE_FROM_ORGANIZATION];
 
 type Props = {
