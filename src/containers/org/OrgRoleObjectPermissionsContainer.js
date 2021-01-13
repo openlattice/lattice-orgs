7c16d1b20cd0211d03cbcc212f55ba5353cf9156
Todd Bergman <todd@openlattice.com>
2021-01-13 14:03:11 -0800

    Added role object permission modal

--- src/containers/org/OrgRoleObjectPermissionsContainer.js
+++ src/containers/org/OrgRoleObjectPermissionsContainer.js
@@ -5,7 +5,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
 
 import { List } from 'immutable';
-import { AppContentWrapper, Typography } from 'lattice-ui-kit';
+import { AppContentWrapper, Modal, Typography } from 'lattice-ui-kit';
 import { useRequestState } from 'lattice-utils';
 import { useDispatch, useSelector } from 'react-redux';
 import { RequestStates } from 'redux-reqseq';
@@ -24,6 +24,7 @@ import { resetRequestState } from '../../core/redux/actions';
 import { PERMISSIONS } from '../../core/redux/constants';
 import { selectOrganization } from '../../core/redux/selectors';
 import { ObjectPermissionsContainer, PermissionsActionsGrid } from '../permissions';
+import { AssignPermissionsToRoleObjectModalBody } from '../permissions/assign-permissions-to-role-object';
 
 const OrgRoleObjectPermissionsContainer = ({
   organizationId,
@@ -43,6 +44,7 @@ const OrgRoleObjectPermissionsContainer = ({
 
   const [filterByPermissionTypes, setFilterByPermissionTypes] = useState([]);
   const [filterByQuery, setFilterByQuery] = useState('');
+  const [isVisibleAssignPermissionsModal, setIsVisibleAssignPermissionsModal] = useState(false);
 
   const getOrgRoleObjectPermissionsRS :?RequestState = useRequestState([PERMISSIONS, GET_ORG_ROLE_OBJECT_PERMISSIONS]);
 
@@ -82,7 +84,7 @@ const OrgRoleObjectPermissionsContainer = ({
               assignPermissionsText="Add permission"
               onChangeFilterByPermissionTypes={setFilterByPermissionTypes}
               onChangeFilterByQuery={setFilterByQuery}
-              onClickAssignPermissions={() => {}} />
+              onClickAssignPermissions={() => setIsVisibleAssignPermissionsModal(true)} />
           <Divider isVisible={false} margin={0} />
           <ObjectPermissionsContainer
               filterByPermissionTypes={filterByPermissionTypes}
@@ -90,6 +92,19 @@ const OrgRoleObjectPermissionsContainer = ({
               objectKey={objectKey}
               organizationId={organizationId} />
         </StackGrid>
+        <Modal
+            isVisible={isVisibleAssignPermissionsModal}
+            onClose={() => setIsVisibleAssignPermissionsModal(false)}
+            shouldCloseOnOutsideClick={false}
+            textTitle="Assign Permissions To Role Object"
+            viewportScrolling
+            withFooter={false}>
+          <AssignPermissionsToRoleObjectModalBody
+              onClose={() => setIsVisibleAssignPermissionsModal(false)}
+              objectKey={objectKey}
+              organizationId={organizationId}
+              principal={role.principal} />
+        </Modal>
       </AppContentWrapper>
     );
   }
