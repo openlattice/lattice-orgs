4d4e5873216afb33bc1fe364449505113d68476e
Todd Bergman <todd@openlattice.com>
2021-01-14 11:43:58 -0800

    Refactor for case of dataset

--- src/containers/permissions/StepSelectPermissions.js
+++ src/containers/permissions/StepSelectPermissions.js
@@ -5,14 +5,22 @@
 import React from 'react';
 
 import _capitalize from 'lodash/capitalize';
+import { faToggleOn } from '@fortawesome/pro-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Types } from 'lattice';
-import { Select, Typography } from 'lattice-ui-kit';
+import {
+  Colors,
+  IconButton,
+  Select,
+  Typography
+} from 'lattice-ui-kit';
 import type { PermissionType } from 'lattice';
 
-import { StackGrid } from '../../components';
+import { SpaceBetweenGrid, StackGrid } from '../../components';
 import type { ReactSelectOption } from '../../types';
 
 const { PermissionTypes } = Types;
+const { NEUTRAL, PURPLE } = Colors;
 
 const PERMISSIONS_OPTIONS = [
   { label: _capitalize(PermissionTypes.OWNER), value: PermissionTypes.OWNER },
@@ -23,14 +31,25 @@ const PERMISSIONS_OPTIONS = [
 ];
 
 const StepSelectPermissions = ({
+  assignPermissionsToAllProperties,
+  isDataSet,
+  setAssignPermissionsToAllProperties,
   setTargetPermissionOptions,
   targetTitle,
   targetPermissionOptions,
 } :{
+  assignPermissionsToAllProperties :?boolean;
+  isDataSet :?boolean;
+  setAssignPermissionsToAllProperties :(assignPermissionsToAllProperties :boolean) => void;
   setTargetPermissionOptions :(permissionTypes :ReactSelectOption<PermissionType>[]) => void;
   targetTitle :string;
   targetPermissionOptions :ReactSelectOption<PermissionType>[];
 }) => {
+
+  const permissions = targetPermissionOptions
+    .map((option) => option.value)
+    .map(_capitalize)
+    .join(', ');
 
   const handleOnChange = (options :?ReactSelectOption<PermissionType>[]) => {
     if (!options) {
@@ -51,6 +70,31 @@ const StepSelectPermissions = ({
           onChange={handleOnChange}
           options={PERMISSIONS_OPTIONS}
           value={targetPermissionOptions} />
+      {
+        isDataSet && (
+          <>
+            <Typography>
+              You can assign permissions to either the data set and all properties, or just the data set itself.
+              Permissions on individual properties can be assigned later.
+            </Typography>
+            <SpaceBetweenGrid>
+              <Typography variant="body1">
+                {`Assign ${permissions} to all properties:`}
+              </Typography>
+              <IconButton
+                  aria-label="permissions toggle for all properties"
+                  onClick={() => setAssignPermissionsToAllProperties(!assignPermissionsToAllProperties)}>
+                <FontAwesomeIcon
+                    color={assignPermissionsToAllProperties ? PURPLE.P300 : NEUTRAL.N500}
+                    fixedWidth
+                    icon={faToggleOn}
+                    transform={{ rotate: assignPermissionsToAllProperties ? 0 : 180 }}
+                    size="lg" />
+              </IconButton>
+            </SpaceBetweenGrid>
+          </>
+        )
+      }
     </StackGrid>
   );
 };
