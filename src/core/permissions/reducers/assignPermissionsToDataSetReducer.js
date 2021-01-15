5dde29fb18e7021a5d212884163aaa8a0f3970f3
Todd Bergman <todd@openlattice.com>
2021-01-15 11:07:51 -0800

    use imported util for updated aces

--- src/core/permissions/reducers/assignPermissionsToDataSetReducer.js
+++ src/core/permissions/reducers/assignPermissionsToDataSetReducer.js
@@ -2,63 +2,29 @@
  * @flow
  */
 
-import { List, Map, Set } from 'immutable';
-import { Models } from 'lattice';
+import { Map } from 'immutable';
 import { RequestStates } from 'redux-reqseq';
+import { Types } from 'lattice';
 import type { SequenceAction } from 'redux-reqseq';
-import type {
-  Ace,
-  PermissionType,
-  UUID,
-} from 'lattice';
 
 import { ACES, ERROR, REQUEST_STATE } from '../../redux/constants';
 import {
   ASSIGN_PERMISSIONS_TO_DATA_SET,
   assignPermissionsToDataSet,
 } from '../actions';
+import { updatePermissionsInState } from '../utils';
 
-const { AceBuilder } = Models;
-
+const { ActionTypes } = Types;
 export default function reducer(state :Map, action :SequenceAction) {
 
-  // TODO: update stored aces
   return assignPermissionsToDataSet.reducer(state, action, {
     REQUEST: () => state
       .setIn([ASSIGN_PERMISSIONS_TO_DATA_SET, REQUEST_STATE], RequestStates.PENDING)
       .setIn([ASSIGN_PERMISSIONS_TO_DATA_SET, action.id], action),
     SUCCESS: () => {
       const { permissions } = action.value;
-      const aces = state.get(ACES).withMutations((mutableAces :Map<List<UUID>, List<Ace>>) => {
-        permissions.forEach((ace :Ace, key :List<UUID>) => {
-          mutableAces.update(key, (storedAces :List<Ace>) => {
-
-            const targetIndex :number = storedAces.findIndex((storedAce :Ace) => (
-              storedAce.principal.id === ace.principal.id && storedAce.principal.type === ace.principal.type
-            ));
-
-            if (targetIndex >= 0) {
-              const targetAce :Ace = storedAces.get(targetIndex);
-
-              let updatedPermissions :Set<PermissionType> = Set(targetAce.permissions);
-              updatedPermissions = updatedPermissions.union(ace.permissions);
-
-              const updatedAce :Ace = (new AceBuilder())
-                .setPermissions(updatedPermissions)
-                .setPrincipal(targetAce.principal)
-                .build();
-
-              return storedAces.set(targetIndex, updatedAce);
-            }
-
-            if (targetIndex === -1) {
-              return storedAces.push(ace);
-            }
-
-            return storedAces;
-          });
-        });
-      });
+      const stateAces = state.get(ACES);
+      const aces = updatePermissionsInState(ActionTypes.ADD, permissions, stateAces);
       return state
         .set(ACES, aces)
         .setIn([ASSIGN_PERMISSIONS_TO_DATA_SET, REQUEST_STATE], RequestStates.SUCCESS);
--- src/core/permissions/reducers/updatePermissionsReducer.js
+++ src/core/permissions/reducers/updatePermissionsReducer.js
@@ -2,13 +2,11 @@
  * @flow
  */
 
-import { List, Map, Set } from 'immutable';
-import { Models, Types } from 'lattice';
+import { List, Map } from 'immutable';
 import { RequestStates } from 'redux-reqseq';
 import type {
   Ace,
   ActionType,
-  PermissionType,
   UUID,
 } from 'lattice';
 import type { SequenceAction } from 'redux-reqseq';
@@ -22,9 +20,7 @@ import {
   UPDATE_PERMISSIONS,
   updatePermissions,
 } from '../actions';
-
-const { AceBuilder } = Models;
-const { ActionTypes } = Types;
+import { updatePermissionsInState } from '../utils';
 
 export default function reducer(state :Map, action :SequenceAction) {
 
@@ -44,42 +40,8 @@ export default function reducer(state :Map, action :SequenceAction) {
           permissions :Map<List<UUID>, Ace>;
         } = storedAction.value;
 
-        const aces = state.get(ACES).withMutations((mutableAces :Map<List<UUID>, List<Ace>>) => {
-          permissions.forEach((ace :Ace, key :List<UUID>) => {
-            mutableAces.update(key, (storedAces :List<Ace>) => {
-
-              const targetIndex :number = storedAces.findIndex((storedAce :Ace) => (
-                storedAce.principal.id === ace.principal.id && storedAce.principal.type === ace.principal.type
-              ));
-
-              if (targetIndex >= 0) {
-                const targetAce :Ace = storedAces.get(targetIndex);
-
-                let updatedPermissions :Set<PermissionType> = Set(targetAce.permissions);
-                if (actionType === ActionTypes.ADD) {
-                  updatedPermissions = updatedPermissions.union(ace.permissions);
-                }
-                else if (actionType === ActionTypes.REMOVE) {
-                  updatedPermissions = updatedPermissions.subtract(ace.permissions);
-                }
-
-                const updatedAce :Ace = (new AceBuilder())
-                  .setPermissions(updatedPermissions)
-                  .setPrincipal(targetAce.principal)
-                  .build();
-
-                return storedAces.set(targetIndex, updatedAce);
-              }
-
-              if (targetIndex === -1 && actionType === ActionTypes.ADD) {
-                return storedAces.push(ace);
-              }
-
-              return storedAces;
-            });
-          });
-        });
-
+        const stateAces = state.get(ACES);
+        const aces = updatePermissionsInState(actionType, permissions, stateAces);
         return state
           .set(ACES, aces)
           .setIn([UPDATE_PERMISSIONS, REQUEST_STATE], RequestStates.SUCCESS);
