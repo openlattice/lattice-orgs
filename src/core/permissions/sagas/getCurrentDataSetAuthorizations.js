5f4eca117bba67cebd20dcea2ec064a5ed4c7f22
Todd Bergman <todd@openlattice.com>
2021-02-03 17:05:48 -0800

    update getCurrentDataSetAuthorizations to take a withProperties prop

--- src/core/permissions/sagas/getCurrentDataSetAuthorizations.js
+++ src/core/permissions/sagas/getCurrentDataSetAuthorizations.js
@@ -5,16 +5,18 @@
 import {
   call,
   put,
+  select,
   takeEvery,
 } from '@redux-saga/core/effects';
-import { List, Map } from 'immutable';
+import { List, Map, Set } from 'immutable';
 import { Models } from 'lattice';
 import {
   AuthorizationsApiActions,
   AuthorizationsApiSagas,
 } from 'lattice-sagas';
-import { Logger } from 'lattice-utils';
+import { Logger, ReduxUtils } from 'lattice-utils';
 import type { Saga } from '@redux-saga/core';
+import type { EntitySet, EntityType, UUID } from 'lattice';
 import type { WorkerResponse } from 'lattice-sagas';
 import type { SequenceAction } from 'redux-reqseq';
 
@@ -27,27 +29,61 @@ const { AccessCheck, AccessCheckBuilder } = Models;
 const { getAuthorizations } = AuthorizationsApiActions;
 const { getAuthorizationsWorker } = AuthorizationsApiSagas;
 
+const { selectEntitySets, selectEntityTypes } = ReduxUtils;
+
 function* getCurrentDataSetAuthorizationsWorker(action :SequenceAction) :Saga<WorkerResponse> {
 
   let workerResponse :WorkerResponse;
 
   try {
     yield put(getCurrentDataSetAuthorizations.request(action.id, action.value));
-    const { aclKey, permissions } = action.value;
+    const { aclKey, permissions, withProperties } = action.value;
 
-    const accessChecks :AccessCheck[] = [
-      (new AccessCheckBuilder())
-        .setAclKey(aclKey)
-        .setPermissions(permissions)
-        .build()
-    ];
+    let entitySets :Map<UUID, EntitySet> = Map();
+    let entityTypes :Map<UUID, EntityType> = Map();
+    if (withProperties) {
+      entitySets = yield select(selectEntitySets(aclKey));
+      entityTypes = yield select(
+        selectEntityTypes(
+          entitySets.valueSeq().map((entitySet :EntitySet) => entitySet.entityTypeId)
+        )
+      );
+    }
+
+    const keys :List<List<UUID>> = List().withMutations((mutableKeys :List<List<UUID>>) => {
+
+      Set(aclKey).forEach((id :UUID) => {
+        mutableKeys.push(List([id]));
+      });
+
+      if (withProperties) {
+        entitySets.forEach((entitySet :EntitySet, entitySetId :UUID) => {
+          const entityType :EntityType = entityTypes.get(entitySet.entityTypeId);
+          entityType.properties.forEach((propertyTypeId :UUID) => {
+            mutableKeys.push(List([entitySetId, propertyTypeId]));
+          });
+        });
+      }
+    });
+
+    const accessChecks :AccessCheck[] = [];
+    keys.forEach((key) => {
+      accessChecks.push(
+        (new AccessCheckBuilder())
+          .setAclKey(key)
+          .setPermissions(permissions)
+          .build()
+      );
+    });
+
     const response :WorkerResponse = yield call(getAuthorizationsWorker, getAuthorizations(accessChecks));
     if (response.error) throw response.error;
-    const authorization :AuthorizationObject = response.data[0];
-
-    workerResponse = {
-      data: Map([[List(authorization.aclKey), authorization.permissions]])
-    };
+    const data = Map().withMutations((mutableMap) => {
+      response.data.forEach((authorization :AuthorizationObject) => {
+        mutableMap.set(List(authorization.aclKey), authorization.permissions);
+      });
+    });
+    workerResponse = { data };
     yield put(getCurrentDataSetAuthorizations.success(action.id, workerResponse.data));
   }
   catch (error) {
