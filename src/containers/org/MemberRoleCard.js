d55ea55a42ac7d7d18ce371645444d85bc1e65b1
Todd Bergman <todd@openlattice.com>
2021-02-05 14:37:54 -0800

    make chip an actual link

--- src/containers/org/MemberRoleCard.js
+++ src/containers/org/MemberRoleCard.js
@@ -3,11 +3,9 @@ import React from 'react';
 
 // $FlowFixMe
 import { Chip } from 'lattice-ui-kit';
-import { useDispatch } from 'react-redux';
 import type { Role, UUID } from 'lattice';
 
 import { ORG_ID_PARAM, ORG_ROLE, ROLE_ID_PARAM } from '../../core/router/Routes';
-import { goToRoute } from '../../core/router/actions';
 
 type Props = {
   onClick :(role :Role) => void;
@@ -22,20 +20,22 @@ const MemberRoleCard = ({
   role,
   isUnassignable,
 } :Props) => {
-  const dispatch = useDispatch();
   const roleId :UUID = role.id || '';
   const rolePath = ORG_ROLE
     .replace(ORG_ID_PARAM, organizationId)
     .replace(ROLE_ID_PARAM, roleId);
-
-  const goToRolePath = () => dispatch(goToRoute(rolePath));
 
   const handleClick = () => {
     onClick(role);
   };
 
   return (
-    <Chip label={role.title} onClick={goToRolePath} onDelete={isUnassignable ? handleClick : undefined} />
+    <Chip
+        clickable
+        component="a"
+        href={`#${rolePath}`}
+        label={role.title}
+        onDelete={isUnassignable ? handleClick : undefined} />
   );
 };
 
