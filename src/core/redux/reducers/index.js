6daed633be4b336928dfbf5b17a997f6049af519
Todd Bergman <todd@openlattice.com>
2021-04-26 09:30:12 -0700

    add resetRequestState

--- src/core/redux/reducers/index.js
+++ src/core/redux/reducers/index.js
@@ -2,6 +2,5 @@
  * @flow
  */
 
-/* eslint-disable import/prefer-default-export */
-
-export { default as resetRequestStatesReducer } from './resetRequestStatesReducer';
+export { default as resetRequestStateReducer } from './resetRequestState';
+export { default as resetRequestStatesReducer } from './resetRequestStates';
