e0446bf0178df3b63d1121037eb42757984428f8
Todd Bergman <todd@openlattice.com>
2021-04-26 09:29:55 -0700

    update name

--- src/core/redux/reducers/resetRequestStates.js
+++ src/core/redux/reducers/resetRequestStates.js
@@ -4,16 +4,19 @@
 
 import { Map } from 'immutable';
 import { RequestStates } from 'redux-reqseq';
+import { LangUtils } from 'lattice-utils';
 
 import { ERROR, REQUEST_STATE } from '../constants';
 import type { ResetRequestStatesAction } from '../actions';
+
+const { isNonEmptyString } = LangUtils;
 
 export default function reducer(state :Map, action :ResetRequestStatesAction) {
 
   const { actions = [] } = action;
   let newState = state;
   actions.forEach((requestStateAction :string) => {
-    if (state.hasIn([requestStateAction])) {
+    if (isNonEmptyString(requestStateAction) && state.hasIn([requestStateAction])) {
       newState = newState
         .setIn([requestStateAction, ERROR], false)
         .setIn([requestStateAction, REQUEST_STATE], RequestStates.STANDBY);
