67c6fdac8d931c3a12cd1b8708228a1a50385f28
Todd Bergman <todd@openlattice.com>
2021-04-30 09:20:03 -0700

    renamed and moved from data directory

---
+++ src/core/redux/selectors/selectOrgEntitySetData.js
@@ -0,0 +1,18 @@
+/*
+ * @flow
+ */
+import { Collection, Map } from 'immutable';
+import type { UUID } from 'lattice';
+
+import { DATA, ENTITY_SET_DATA } from '../../redux/constants';
+
+export default function selectOrgEntitySetData(entitySetId :UUID, entityKeyIds :Collection<UUID> | UUID[]) :Map {
+  return ((state :Map) => {
+    const entitySetData :Map = state.getIn([DATA, ENTITY_SET_DATA, entitySetId], Map());
+    return Map().withMutations((map) => {
+      entityKeyIds.forEach((entityKeyId :UUID) => {
+        map.set(entityKeyId, entitySetData.get(entityKeyId, Map()));
+      });
+    });
+  });
+}
