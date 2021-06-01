fc28f727bd275f7ebfa6112d23daead3d9cd6766
Todd Bergman <todd@openlattice.com>
2021-06-01 19:40:21 -0400

    add EntityDataContainer to modal

--- src/containers/explore/components/EntityDataModal.js
+++ src/containers/explore/components/EntityDataModal.js
@@ -2,38 +2,31 @@
  * @flow
  */
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { Map } from 'immutable';
 import styled from 'styled-components';
 import {
   Banner,
   Colors,
-  Input,
   Modal,
   ModalHeader,
   Spinner,
   Typography
 } from 'lattice-ui-kit';
-import { ReduxUtils, ValidationUtils, useRequestState } from 'lattice-utils';
-import { useDispatch, useSelector } from 'react-redux';
+import { ReduxUtils, useRequestState } from 'lattice-utils';
 import type { UUID } from 'lattice';
 import type { RequestState } from 'redux-reqseq';
 
-import { ModalBody, ResetOnUnmount } from '../../../components';
-import { resetRequestStates } from '../../../core/redux/actions';
+import { ModalBody } from '../../../components';
+import EntityDataContainer from '../EntityDataContainer';
 import {
   EXPLORE_ENTITY_DATA,
-  EXPLORE_ENTITY_NEIGHBORS,
-  exploreEntityData,
-  exploreEntityNeighbors
+  EXPLORE_ENTITY_NEIGHBORS
 } from '../actions';
-import { selectOrgDataSetColumns } from '../../../core/redux/selectors';
-import { EXPLORE, ENTITY_NEIGHBORS_MAP, SELECTED_ENTITY_DATA } from '../../../core/redux/constants';
+import { EXPLORE } from '../../../core/redux/constants';
 
 const { NEUTRAL } = Colors;
 const { isPending, reduceRequestStates } = ReduxUtils;
-const { isValidUUID } = ValidationUtils;
 
 const StyledModalHeader = styled(ModalHeader)`
   background: ${NEUTRAL.N600};
@@ -44,8 +37,9 @@ const StyledModalHeader = styled(ModalHeader)`
   }
 `;
 
-const EntityKeyId = styled(Typography)`
-  padding: 20px 0;
+const StyledModalBody = styled(ModalBody)`
+  height: 90vh;
+  width: 70vw;
 `;
 
 const DeleteOrgModal = ({
@@ -63,27 +57,12 @@ const DeleteOrgModal = ({
   onClose :() => void;
   organizationId :UUID;
 |}) => {
-  const dispatch = useDispatch();
-
   const exploreEntityDataRS :?RequestState = useRequestState([EXPLORE, EXPLORE_ENTITY_DATA, entityKeyId]);
   const exploreEntityNeighborsRS :?RequestState = useRequestState([EXPLORE, EXPLORE_ENTITY_NEIGHBORS, entityKeyId]);
   const reducedRequestState :?RequestState = reduceRequestStates([exploreEntityDataRS, exploreEntityNeighborsRS]);
 
-  const dataSetColumns :List<Map<FQN, List>> = useSelector(selectOrgDataSetColumns(organizationId, dataSetId));
-  const neighbors :?Map = useSelector((s) => s.getIn([EXPLORE, ENTITY_NEIGHBORS_MAP, entityKeyId], Map()));
-  const entityData :?Map = useSelector((s) => s.getIn([EXPLORE, SELECTED_ENTITY_DATA], Map()));
-
-  useEffect(() => {
-    if (isValidUUID(entityKeyId) && isValidUUID(dataSetId)) {
-      dispatch(exploreEntityData({ entityKeyId, entitySetId: dataSetId }));
-      dispatch(exploreEntityNeighbors({ entityKeyId, entitySetId: dataSetId }));
-    }
-  }, [dispatch, entityKeyId, dataSetId]);
-
   const header = (
-    <StyledModalHeader textTitle={`Examining ${dataSetName}: ${entityKeyId}`} onClose={onClose}>
-      <Banner isOpen>{`Examining ${dataSetName}: ${entityKeyId}`}</Banner>
-    </StyledModalHeader>
+    <StyledModalHeader textTitle={`Examining ${dataSetName}: ${dataSetId}`} onClose={onClose} />
   );
 
   return (
@@ -97,9 +76,9 @@ const DeleteOrgModal = ({
             <Spinner />
           )
           : (
-            <ModalBody>
-              <EntityKeyId variant="h1">{entityKeyId}</EntityKeyId>
-            </ModalBody>
+            <StyledModalBody>
+              <EntityDataContainer dataSetId={dataSetId} entityKeyId={entityKeyId} organizationId={organizationId} />
+            </StyledModalBody>
           )
       }
     </Modal>
