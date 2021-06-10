25af6592a681be73970c13ef9aa6076e35cae065
Todd Bergman <todd@openlattice.com>
2021-06-10 11:01:59 -0700

    created selectSelectedEntityData

---
+++ src/core/redux/selectors/selectSelectedEntityData.js
@@ -0,0 +1,13 @@
+/*
+ * @flow
+ */
+
+import { Map } from 'immutable';
+
+import { EXPLORE, SELECTED_ENTITY_DATA } from '../constants';
+
+export default function selectEntityNeighborsMap() {
+  return (state :Map) :Map => (
+    state.getIn([EXPLORE, SELECTED_ENTITY_DATA], Map())
+  );
+}
