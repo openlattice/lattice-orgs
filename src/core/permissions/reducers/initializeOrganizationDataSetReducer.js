4ab61c241425ed98eee453abbfe3a847284564f8
Hristo Oskov <hristo@openlattice.com>
2021-06-11 12:01:43 -0700

    migrate permissions/reducers/initializeOrganizationDataSetReducer

--- src/core/permissions/reducers/initializeOrganizationDataSetReducer.js
+++ src/core/permissions/reducers/initializeOrganizationDataSetReducer.js
@@ -2,23 +2,19 @@
  * @flow
  */
 
-import { List, Map } from 'immutable';
-import { DataUtils } from 'lattice-utils';
+import { List, Map, get } from 'immutable';
 import type { UUID } from 'lattice';
 import type { SequenceAction } from 'redux-reqseq';
 
 import { initializeOrganizationDataSet } from '../../edm/actions';
-import { FQNS } from '../../edm/constants';
 import { DATA_SET, IS_OWNER, MY_KEYS } from '../../redux/constants';
-
-const { getPropertyValue } = DataUtils;
 
 export default function reducer(state :Map, action :SequenceAction) {
 
   return initializeOrganizationDataSet.reducer(state, action, {
     SUCCESS: () => {
       if (action.value[IS_OWNER] === true) {
-        const dataSetId :UUID = getPropertyValue(action.value[DATA_SET], [FQNS.OL_ID, 0]);
+        const dataSetId :UUID = get(action.value[DATA_SET], 'id');
         const keys :List<List<UUID>> = List().push(List([dataSetId]));
         return state.mergeIn([MY_KEYS], keys);
       }
