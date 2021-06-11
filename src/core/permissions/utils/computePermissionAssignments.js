c45380efda57bee6598b7d532105e055d86c0015
Hristo Oskov <hristo@openlattice.com>
2021-06-11 14:55:48 -0700

    migrate src/core/permissions/utils/computePermissionAssignments

--- src/core/permissions/utils/computePermissionAssignments.js
+++ src/core/permissions/utils/computePermissionAssignments.js
@@ -3,22 +3,16 @@
  */
 
 import { List, Map, Set } from 'immutable';
-import { DataUtils } from 'lattice-utils';
 import type {
   Ace,
-  FQN,
   PermissionType,
   PropertyType,
   UUID,
 } from 'lattice';
 
-import { FQNS } from '../../edm/constants';
-
-const { getPropertyValue } = DataUtils;
-
 function computePermissionAssignments(
   myKeys :Set<List<UUID>>,
-  dataSetColumns :List<Map<FQN, List>>,
+  dataSetColumns :List<Map>,
   dataSetId :UUID,
   permissions :Map<List<UUID>, Ace>,
   permissionType :PermissionType,
@@ -33,8 +27,8 @@ function computePermissionAssignments(
   let isAssignedToOnlyNonPII = true;
   let isOwnerOfAtLeastOneColumn = false;
 
-  dataSetColumns.forEach((column :Map<FQN, List>) => {
-    const columnId :UUID = getPropertyValue(column, [FQNS.OL_ID, 0]);
+  dataSetColumns.forEach((column :Map) => {
+    const columnId :UUID = column.get('id');
     const propertyType :?PropertyType = maybePropertyTypes.get(columnId);
     const pii :boolean = propertyType?.pii || false;
     const key :List<UUID> = List([dataSetId, columnId]);
