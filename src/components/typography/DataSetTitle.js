8f5c260352f2cfff91fe5bb29ecc908a30916913
Hristo Oskov <hristo@openlattice.com>
2021-06-11 15:00:51 -0700

    migrate src/components/typography/DataSetTitle

--- src/components/typography/DataSetTitle.js
+++ src/components/typography/DataSetTitle.js
@@ -5,15 +5,11 @@
 import React from 'react';
 
 import { Typography } from 'lattice-ui-kit';
-import { DataUtils } from 'lattice-utils';
 import type { Map } from 'immutable';
 
 import { AtlasDataSetIcon, EntitySetIcon } from '../../assets/svg/icons';
-import { FQNS } from '../../core/edm/constants';
 import { isAtlasDataSet } from '../../utils';
 import { GapGrid } from '../grids';
-
-const { getPropertyValue } = DataUtils;
 
 const DataSetTitle = ({
   component,
@@ -32,8 +28,7 @@ const DataSetTitle = ({
     }
     <Typography component={component} variant={variant}>
       {
-        getPropertyValue(dataSet, [FQNS.OL_TITLE, 0])
-        || getPropertyValue(dataSet, [FQNS.OL_DATA_SET_NAME, 0])
+        dataSet.getIn(['metadata', 'title']) || dataSet.get('name')
       }
     </Typography>
   </GapGrid>
