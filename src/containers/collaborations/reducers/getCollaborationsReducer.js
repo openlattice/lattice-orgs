7bced6e9db9bc64d1cb086026f76d9d88b9e6630
Todd Bergman <todd@openlattice.com>
2021-06-14 14:56:42 -0700

    created getCollaborationsReducer

---
+++ src/containers/collaborations/reducers/getCollaborationsReducer.js
@@ -0,0 +1,44 @@
+/*
+ * @flow
+ */
+
+import { Map, fromJS } from 'immutable';
+import { CollaborationsApiActions } from 'lattice-sagas';
+import { RequestStates } from 'redux-reqseq';
+import type { UUID } from 'lattice';
+import type { SequenceAction } from 'redux-reqseq';
+
+import { COLLABORATIONS, ERROR, REQUEST_STATE } from '../../../core/redux/constants';
+
+const { GET_COLLABORATIONS, getCollaborations } = CollaborationsApiActions;
+
+export default function getCollaborationsReducer(state :Map, action :SequenceAction) {
+  return getCollaborations.reducer(state, action, {
+    REQUEST: () => state
+      .setIn([GET_COLLABORATIONS, REQUEST_STATE], RequestStates.PENDING)
+      .setIn([GET_COLLABORATIONS, action.id], action),
+    SUCCESS: () => {
+      if (state.hasIn([GET_COLLABORATIONS, action.id])) {
+        // TODO: Add Collaboration Model to lattice-js
+        const collaborations :Map<UUID, Map> = Map().asMutable();
+        action.value.forEach((collaboration :Object) => {
+          collaborations.set(collaboration.id, fromJS(collaboration));
+        });
+        return state
+          .set(COLLABORATIONS, collaborations.asImmutable())
+          .setIn([GET_COLLABORATIONS, REQUEST_STATE], RequestStates.SUCCESS);
+      }
+      return state;
+    },
+    FAILURE: () => {
+      if (state.hasIn([GET_COLLABORATIONS, action.id])) {
+        return state
+          .set(COLLABORATIONS, Map())
+          .setIn([GET_COLLABORATIONS, ERROR], action.value)
+          .setIn([GET_COLLABORATIONS, REQUEST_STATE], RequestStates.FAILURE);
+      }
+      return state;
+    },
+    FINALLY: () => state.deleteIn([GET_COLLABORATIONS, action.id]),
+  });
+}
