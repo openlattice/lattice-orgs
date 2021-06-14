afdbdd958e78abc31f1fb321470eca03936c2dc7
Todd Bergman <todd@openlattice.com>
2021-06-14 09:31:25 -0700

    Update src/core/redux/selectors/selectSelectedEntityData.js
    Co-authored-by: Hristo Oskov <UnsungHero97@users.noreply.github.com>

--- src/core/redux/selectors/selectSelectedEntityData.js
+++ src/core/redux/selectors/selectSelectedEntityData.js
@@ -6,7 +6,7 @@ import { Map } from 'immutable';
 
 import { EXPLORE, SELECTED_ENTITY_DATA } from '../constants';
 
-export default function selectEntityNeighborsMap() {
+export default function selectSelectedEntityData() {
   return (state :Map) :Map => (
     state.getIn([EXPLORE, SELECTED_ENTITY_DATA], Map())
   );
