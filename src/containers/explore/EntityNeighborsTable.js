464ad88edfc1bef4203916d897fc63d38040f984
Todd Bergman <todd@openlattice.com>
2021-06-14 09:31:07 -0700

    Update src/containers/explore/EntityNeighborsTable.js
    Co-authored-by: Hristo Oskov <UnsungHero97@users.noreply.github.com>

--- src/containers/explore/EntityNeighborsTable.js
+++ src/containers/explore/EntityNeighborsTable.js
@@ -63,7 +63,7 @@ const EntityNeighborsTable = ({
   const dispatch = useDispatch();
   const [tableData, setTableData] = useState([]);
   const [tablePage, setTablePage] = useState(0);
-  const [neighborsIndex, setNeighborsIndex] = useState(0);
+  const [neighborsIndex, setNeighborsIndex] = useState(1);
   const fetchEntitySetDataRS :?RequestState = useRequestState([DATA, FETCH_ENTITY_SET_DATA, dataSetId]);
 
   const neighborToAssociationEKIDs :Map = useMemo(() => (
