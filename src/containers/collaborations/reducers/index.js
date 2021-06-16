225a84256afa79c79da5f940d2084a0faf2e277a
Todd Bergman <todd@openlattice.com>
2021-06-16 11:21:19 -0700

    added deleteCollaboration

--- src/containers/collaborations/reducers/index.js
+++ src/containers/collaborations/reducers/index.js
@@ -6,6 +6,7 @@ import { Map, fromJS } from 'immutable';
 import { CollaborationsApiActions } from 'lattice-sagas';
 
 import createNewCollaborationReducer from './createNewCollaborationReducer';
+import deleteCollaborationReducer from './deleteCollaborationReducer';
 import getCollaborationsReducer from './getCollaborationsReducer';
 
 import { RESET_REQUEST_STATES } from '../../../core/redux/actions';
@@ -18,13 +19,16 @@ import { resetRequestStatesReducer } from '../../../core/redux/reducers';
 import { CREATE_NEW_COLLABORATION, createNewCollaboration } from '../actions';
 
 const {
+  DELETE_COLLABORATION,
   GET_COLLABORATIONS,
+  deleteCollaboration,
   getCollaborations,
 } = CollaborationsApiActions;
 
 const INITIAL_STATE :Map = fromJS({
   // actions
   [CREATE_NEW_COLLABORATION]: RS_INITIAL_STATE,
+  [DELETE_COLLABORATION]: RS_INITIAL_STATE,
   [GET_COLLABORATIONS]: RS_INITIAL_STATE,
   // data
   [COLLABORATIONS]: Map(),
@@ -41,6 +45,10 @@ export default function reducer(state :Map = INITIAL_STATE, action :Object) {
 
     case createNewCollaboration.case(action.type): {
       return createNewCollaborationReducer(state, action);
+    }
+
+    case deleteCollaboration.case(action.type): {
+      return deleteCollaborationReducer(state, action);
     }
 
     case getCollaborations.case(action.type): {
