612d94db8fa3fe0f0fdc9831ec53f2b21a41b3b3
Todd Bergman <todd@openlattice.com>
2021-06-15 17:57:41 -0700

    add getUsersCollaborations Selector

--- src/core/redux/selectors/index.js
+++ src/core/redux/selectors/index.js
@@ -39,5 +39,6 @@ export { default as selectSearchQuery } from './selectSearchQuery';
 export { default as selectSearchTotalHits } from './selectSearchTotalHits';
 export { default as selectStoredEntityKeyIds } from './selectStoredEntityKeyIds';
 export { default as selectUser } from './selectUser';
-export { default as selectUserSearchResults } from './selectUserSearchResults';
 export { default as selectUsers } from './selectUsers';
+export { default as selectUsersCollaborations } from './selectUsersCollaborations';
+export { default as selectUserSearchResults } from './selectUserSearchResults';
