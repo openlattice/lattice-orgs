20a176a2c7c7d534807c892d536b17eae0204ad9
Hristo Oskov <hristo@openlattice.com>
2021-06-16 17:54:51 -0700

    getDataSetMetadataReducer

---
+++ src/core/edm/reducers/getDataSetMetadataReducer.js
@@ -0,0 +1,42 @@
+/*
+ * @flow
+ */
+
+import { Map, fromJS } from 'immutable';
+import { DataSetMetadataApiActions } from 'lattice-sagas';
+import { RequestStates } from 'redux-reqseq';
+import type { UUID } from 'lattice';
+import type { SequenceAction } from 'redux-reqseq';
+
+import { ERROR, ORG_DATA_SETS, REQUEST_STATE } from '../../redux/constants';
+
+const { GET_DATA_SET_METADATA, getDataSetMetadata } = DataSetMetadataApiActions;
+
+export default function reducer(state :Map, action :SequenceAction) {
+
+  return getDataSetMetadata.reducer(state, action, {
+    REQUEST: () => state
+      .setIn([GET_DATA_SET_METADATA, REQUEST_STATE], RequestStates.PENDING)
+      .setIn([GET_DATA_SET_METADATA, action.id], action),
+    SUCCESS: () => {
+      if (state.hasIn([GET_DATA_SET_METADATA, action.id])) {
+        const dataSet :Map = fromJS(action.value);
+        const dataSetId :UUID = dataSet.get('id');
+        const organizationId :UUID = dataSet.get('organizationId');
+        return state
+          .setIn([ORG_DATA_SETS, organizationId, dataSetId], dataSet)
+          .setIn([GET_DATA_SET_METADATA, REQUEST_STATE], RequestStates.SUCCESS);
+      }
+      return state;
+    },
+    FAILURE: () => {
+      if (state.hasIn([GET_DATA_SET_METADATA, action.id])) {
+        return state
+          .setIn([GET_DATA_SET_METADATA, ERROR], action.value)
+          .setIn([GET_DATA_SET_METADATA, REQUEST_STATE], RequestStates.FAILURE);
+      }
+      return state;
+    },
+    FINALLY: () => state.deleteIn([GET_DATA_SET_METADATA, action.id]),
+  });
+}
