11c71fdf006a64e87ffaf0d364aae40a8ee54164
Todd Bergman <todd@openlattice.com>
2021-06-17 10:06:37 -0700

    added getDataSetsInCollaboration action

--- src/containers/collaborations/actions/index.js
+++ src/containers/collaborations/actions/index.js
@@ -8,7 +8,12 @@ import type { RequestSequence } from 'redux-reqseq';
 const CREATE_NEW_COLLABORATION :'CREATE_NEW_COLLABORATION' = 'CREATE_NEW_COLLABORATION';
 const createNewCollaboration :RequestSequence = newRequestSequence(CREATE_NEW_COLLABORATION);
 
+const GET_DATA_SETS_IN_COLLABORATION :'GET_DATA_SETS_IN_COLLABORATION' = 'GET_DATA_SETS_IN_COLLABORATION';
+const getDataSetsInCollaboration :RequestSequence = newRequestSequence(GET_DATA_SETS_IN_COLLABORATION);
+
 export {
   CREATE_NEW_COLLABORATION,
+  GET_DATA_SETS_IN_COLLABORATION,
   createNewCollaboration,
+  getDataSetsInCollaboration,
 };
