6f0074de045526655f910524b938a2e7c1dc9ca0
Todd Bergman <todd@openlattice.com>
2021-06-17 20:59:48 -0700

    added getCollaborationDatabaseInfo

--- src/containers/collaborations/reducers/index.js
+++ src/containers/collaborations/reducers/index.js
@@ -9,14 +9,15 @@ import addDataSetToCollaborationReducer from './addDataSetToCollaborationReducer
 import addOrganizationsToCollaborationReducer from './addOrganizationsToCollaborationReducer';
 import createNewCollaborationReducer from './createNewCollaborationReducer';
 import deleteCollaborationReducer from './deleteCollaborationReducer';
+import getCollaborationDatabaseInfoReducer from './getCollaborationDatabaseInfoReducer';
 import getCollaborationsReducer from './getCollaborationsReducer';
 import getDataSetsInCollaborationReducer from './getDataSetsInCollaborationReducer';
 import removeOrganizationsFromCollaborationReducer from './removeOrganizationsFromCollaborationReducer';
 
 import { RESET_REQUEST_STATES } from '../../../core/redux/actions';
 import {
-  COLLABORATION_DATA_SETS,
   COLLABORATIONS,
+  COLLABORATION_DATA_SETS,
   DATABASE_DETAILS,
   RS_INITIAL_STATE,
 } from '../../../core/redux/constants';
@@ -32,11 +33,13 @@ const {
   ADD_DATA_SET_TO_COLLABORATION,
   ADD_ORGANIZATIONS_TO_COLLABORATION,
   DELETE_COLLABORATION,
+  GET_COLLABORATION_DATABASE_INFO,
   GET_COLLABORATIONS,
   REMOVE_ORGANIZATIONS_FROM_COLLABORATION,
   addDataSetToCollaboration,
   addOrganizationsToCollaboration,
   deleteCollaboration,
+  getCollaborationDatabaseInfo,
   getCollaborations,
   removeOrganizationsFromCollaboration
 } = CollaborationsApiActions;
@@ -47,8 +50,9 @@ const INITIAL_STATE :Map = fromJS({
   [ADD_ORGANIZATIONS_TO_COLLABORATION]: RS_INITIAL_STATE,
   [CREATE_NEW_COLLABORATION]: RS_INITIAL_STATE,
   [DELETE_COLLABORATION]: RS_INITIAL_STATE,
-  [GET_DATA_SETS_IN_COLLABORATION]: RS_INITIAL_STATE,
+  [GET_COLLABORATION_DATABASE_INFO]: RS_INITIAL_STATE,
   [GET_COLLABORATIONS]: RS_INITIAL_STATE,
+  [GET_DATA_SETS_IN_COLLABORATION]: RS_INITIAL_STATE,
   [REMOVE_ORGANIZATIONS_FROM_COLLABORATION]: RS_INITIAL_STATE,
   // data
   [COLLABORATION_DATA_SETS]: Map(),
@@ -86,6 +90,10 @@ export default function reducer(state :Map = INITIAL_STATE, action :Object) {
 
     case getDataSetsInCollaboration.case(action.type): {
       return getDataSetsInCollaborationReducer(state, action);
+    }
+
+    case getCollaborationDatabaseInfo.case(action.type): {
+      return getCollaborationDatabaseInfoReducer(state, action);
     }
 
     case removeOrganizationsFromCollaboration.case(action.type): {
