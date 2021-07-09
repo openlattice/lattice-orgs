7f4bd58e409fedfcf5f2be539f81ee769567e41f
Todd Bergman <todd@openlattice.com>
2021-07-09 11:13:55 -0700

    remove org datasets from collaboration

--- src/containers/collaborations/reducers/removeOrganizationsFromCollaborationReducer.js
+++ src/containers/collaborations/reducers/removeOrganizationsFromCollaborationReducer.js
@@ -5,12 +5,14 @@
 import { List, Map } from 'immutable';
 import { CollaborationsApiActions } from 'lattice-sagas';
 import { RequestStates } from 'redux-reqseq';
+import type { UUID } from 'lattice';
 import type { SequenceAction } from 'redux-reqseq';
 
 import {
   COLLABORATIONS,
-  ORGANIZATION_IDS,
+  COLLABORATION_DATA_SETS,
   ERROR,
+  ORGANIZATION_IDS,
   REQUEST_STATE
 } from '../../../core/redux/constants';
 
@@ -25,7 +27,10 @@ export default function removeOrganiztionsToCollaborationReducer(state :Map, act
       const storedSeqAction = state.getIn([REMOVE_ORGANIZATIONS_FROM_COLLABORATION, action.id]);
       if (storedSeqAction) {
         const { collaborationId, organizationIds } = storedSeqAction.value;
+        const collaborationDataSets :Map<UUID, List> = state.getIn([COLLABORATION_DATA_SETS, collaborationId])
+          .deleteAll(organizationIds);
         return state
+          .setIn([COLLABORATION_DATA_SETS, collaborationId], collaborationDataSets)
           .updateIn(
             [COLLABORATIONS, collaborationId, ORGANIZATION_IDS],
             List(),
