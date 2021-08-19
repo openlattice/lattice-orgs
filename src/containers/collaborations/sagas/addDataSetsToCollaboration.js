ab1ed4af8a855f415a8ce5a5d5e64058a2160083
Todd Bergman <todd@openlattice.com>
2021-08-19 14:13:41 -0700

    create addDataSetsToCollaboration to Sagas

---
+++ src/containers/collaborations/sagas/addDataSetsToCollaboration.js
@@ -0,0 +1,77 @@
+/*
+ * @flow
+ */
+
+import {
+  all,
+  call,
+  put,
+  takeEvery,
+} from '@redux-saga/core/effects';
+import { List } from 'immutable';
+import { CollaborationsApiActions, CollaborationsApiSagas } from 'lattice-sagas';
+import { AxiosUtils, LangUtils, Logger } from 'lattice-utils';
+import type { Saga } from '@redux-saga/core';
+import type { UUID } from 'lattice';
+import type { WorkerResponse } from 'lattice-sagas';
+import type { SequenceAction } from 'redux-reqseq';
+
+import { ADD_DATA_SETS_TO_COLLABORATION, addDataSetsToCollaboration } from '../actions';
+
+const { isDefined } = LangUtils;
+
+const LOG = new Logger('CollaborationSagas');
+
+const { addDataSetToCollaboration } = CollaborationsApiActions;
+const { addDataSetToCollaborationWorker } = CollaborationsApiSagas;
+
+function* addDataSetsToCollaborationWorker(action :SequenceAction) :Saga<*> {
+
+  try {
+    yield put(addDataSetsToCollaboration.request(action.id, action.value));
+
+    const { collaborationId, dataSetIdsByOrgId } = action.value;
+
+    const addDataSetCalls = [];
+    dataSetIdsByOrgId.forEach((dataSetIds :List<UUID>, organizationId :UUID) => {
+      dataSetIds.forEach((dataSetId :UUID) => {
+        addDataSetCalls.push(
+          call(
+            addDataSetToCollaborationWorker,
+            addDataSetToCollaboration({
+              collaborationId,
+              dataSetId,
+              organizationId
+            })
+          )
+        );
+      });
+    });
+
+    const responses :WorkerResponse[] = yield all(addDataSetCalls);
+    const responseError = responses.reduce(
+      (error :any, r :Object) => (isDefined(error) ? error : r.error),
+      undefined,
+    );
+    if (responseError) throw responseError;
+
+    yield put(addDataSetsToCollaboration.success(action.id));
+  }
+  catch (error) {
+    LOG.error(action.type, error);
+    yield put(addDataSetsToCollaboration.failure(action.id, AxiosUtils.toSagaError(error)));
+  }
+  finally {
+    yield put(addDataSetsToCollaboration.finally(action.id));
+  }
+}
+
+function* addDataSetsToCollaborationWatcher() :Saga<*> {
+
+  yield takeEvery(ADD_DATA_SETS_TO_COLLABORATION, addDataSetsToCollaborationWorker);
+}
+
+export {
+  addDataSetsToCollaborationWatcher,
+  addDataSetsToCollaborationWorker,
+};
