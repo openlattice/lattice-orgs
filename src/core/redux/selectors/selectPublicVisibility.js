1e35b76abb4af0ac3f871cb35ec2a62b009e0ee9
Solomon Tang <solomoncztang@gmail.com>
2021-09-13 18:01:13 -0700

    selectPublicVisibility

---
+++ src/core/redux/selectors/selectPublicVisibility.js
@@ -0,0 +1,35 @@
+/*
+ * @flow
+ */
+
+import { List, Map, getIn } from 'immutable';
+import { Types } from 'lattice';
+import { ValidationUtils } from 'lattice-utils';
+import type { UUID } from 'lattice';
+
+import { ACES, AUTHENTICATED_USER, PERMISSIONS } from '../constants';
+
+const { isValidUUID } = ValidationUtils;
+const { PermissionTypes } = Types;
+
+export default function selectPublicVisibility(organizationId :UUID) {
+
+  return (state :Map) :boolean => {
+
+    let isVisible = false;
+    if (isValidUUID(organizationId)) {
+      const orgAce = getIn(state, [PERMISSIONS, ACES, List([organizationId])], List());
+      orgAce.forEach((ace) => {
+        if (ace.principal.id === AUTHENTICATED_USER) {
+          ace.permissions.forEach((permission) => {
+            if (permission === PermissionTypes.READ) {
+              isVisible = true;
+            }
+          });
+        }
+      });
+    }
+
+    return isVisible;
+  };
+}
