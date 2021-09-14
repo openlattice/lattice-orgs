b9e4907a74b750641de30439a131753972ae5248
Solomon Tang <solomoncztang@gmail.com>
2021-09-13 17:59:51 -0700

    set/removePublicVisibility sagas

---
+++ src/containers/org/sagas/removePublicVisibility.js
@@ -0,0 +1,79 @@
+/*
+ * @flow
+ */
+
+import {
+  call,
+  put,
+  takeEvery,
+} from '@redux-saga/core/effects';
+import { Models, Types } from 'lattice';
+import {
+  PermissionsApiActions,
+  PermissionsApiSagas,
+} from 'lattice-sagas';
+import { AxiosUtils, Logger, ValidationUtils } from 'lattice-utils';
+import type { Saga } from '@redux-saga/core';
+import type { UUID } from 'lattice';
+import type { WorkerResponse } from 'lattice-sagas';
+import type { SequenceAction } from 'redux-reqseq';
+
+import createOrganizationVisibilityAcl from '../utils/createOrganizationVisibilityAcl';
+import { ERR_INVALID_UUID } from '../../../utils/constants/errors';
+import { REMOVE_PUBLIC_VISIBILITY, removePublicVisibility } from '../actions';
+
+const {
+  AclData,
+  AclDataBuilder,
+} = Models;
+const { ActionTypes } = Types;
+const { updateAcls } = PermissionsApiActions;
+const { updateAclsWorker } = PermissionsApiSagas;
+const { toSagaError } = AxiosUtils;
+const { isValidUUID } = ValidationUtils;
+
+const LOG = new Logger('PermissionsSagas');
+
+function* removePublicVisibilityWorker(action :SequenceAction) :Saga<WorkerResponse> {
+
+  let workerResponse :WorkerResponse;
+  try {
+    yield put(removePublicVisibility.request(action.id, action.value));
+
+    const organizationId :UUID = action.value;
+    if (!isValidUUID(organizationId)) throw ERR_INVALID_UUID;
+
+    const acl = createOrganizationVisibilityAcl(organizationId);
+
+    const aclData :AclData = (new AclDataBuilder())
+      .setAction(ActionTypes.REMOVE)
+      .setAcl(acl)
+      .build();
+
+    const updateAclResponse :WorkerResponse = yield call(updateAclsWorker, updateAcls([aclData]));
+    if (updateAclResponse.error) throw updateAclResponse.error;
+
+    yield put(removePublicVisibility.success(action.id, acl));
+    workerResponse = { data: acl };
+  }
+  catch (error) {
+    LOG.error(action.type, error);
+    workerResponse = { error };
+    yield put(removePublicVisibility.failure(action.id, toSagaError(error)));
+  }
+  finally {
+    yield put(removePublicVisibility.finally(action.id));
+  }
+
+  return workerResponse;
+}
+
+function* removePublicVisibilityWatcher() :Saga<void> {
+
+  yield takeEvery(REMOVE_PUBLIC_VISIBILITY, removePublicVisibilityWorker);
+}
+
+export {
+  removePublicVisibilityWatcher,
+  removePublicVisibilityWorker,
+};
